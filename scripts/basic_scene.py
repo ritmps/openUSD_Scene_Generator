63f12cd14cad4ac5bda76ef15e02a9017364316c
ZLai <zhenlai1013@gmail.com>
2025-05-15 09:43:08 -0400

    change hdri texture path to absolute for storm renderer

--- scripts/basic_scene.py
+++ scripts/basic_scene.py
@@ -1,4 +1,5 @@
 from usd_scene import SceneBuilder, MaterialLibrary, Environment
+import os
 
 # Create a scene
 builder = SceneBuilder("./outputs/scenes/test_scene.usda")
@@ -6,14 +7,23 @@ materials = MaterialLibrary(builder.stage)
 environment = Environment(builder.stage)
 
 # Create materials and objects
-car_paint = materials.create_car_paint("BluePaint", (0.1, 0.2, 0.8))
-sphere = builder.add_sphere("/World/CarPaintSphere", radius=2.0, material=car_paint)
 
-# Add static camera looking at the sphere
-builder.add_camera("/World/Camera", position=(0, 3, 10), target=(0, 0, 0))
+# # car paint
+# material = materials.create_car_paint("BluePaint", (0.1, 0.2, 0.8))
+
+# glass
+material = materials.create_glass("Glass", roughness = 0.02)
+
+# Create an object at the origin with a selected material
+sphere = builder.add_sphere("/World/Sphere", radius=2.0, material=material)
+
+# Add static camera looking at the object
+camera_position = (-10, -5, -10)
+builder.add_camera("/World/Camera", camera_position, target=(0, 0, 0))
 
 # Set HDRI lighting
-environment.set_hdri_lighting("./assets/textures/studio_env.exr")
+hdri_path = os.path.abspath("./assets/textures/studio_env.exr")
+environment.set_hdri_lighting(hdri_path)
 
 # Save
 builder.save()
